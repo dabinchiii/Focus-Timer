9cef89ae61f14fb29590743d195c93f540536741
dabinchiii <81626630+dabinchiii@users.noreply.github.com>
2025-03-13 17:43:03 +0900

    Feat: 페이지와 GNB의 색상 변경

--- src/components/GNB.tsx
+++ src/components/GNB.tsx
@@ -4,7 +4,7 @@ const GNB = (): React.ReactNode => {
   return (
     <nav>
       <div className="w-full h-[5rem]"></div>
-      <div className="fixed z-50 w-full h-[5rem] top-0 left-1/2 transform -translate-x-1/2 px-4 p-2 border-b">
+      <div className="fixed z-50 w-full h-[5rem] top-0 left-1/2 transform -translate-x-1/2 px-4 p-2 bg-[#C27373] border-b border-white">
         <div className="flex justify-between">
           <a href="./">
             <Image
