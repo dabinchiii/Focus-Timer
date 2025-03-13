085cc21e90d6a6b3e81f6717d28bbc99e5d8bb23
dabinchiii <81626630+dabinchiii@users.noreply.github.com>
2025-03-13 17:29:14 +0900

    Feat: GNB 컴포넌트 생성 및 적용

---
+++ src/components/GNB.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+const GNB = (): React.ReactNode => {
+  return (
+    <nav>
+      <div className="w-full h-[5rem]"></div>
+      <div className="fixed z-50 w-full h-[5rem] top-0 left-1/2 transform -translate-x-1/2 px-4 p-2 border-b">
+        <div className="flex justify-between">
+          <a href="./">
+            <Image
+              src="/images/icons/focustimer_logo.png"
+              alt="Focus Timer"
+              width={160}
+              height={0}
+            />
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default GNB;
