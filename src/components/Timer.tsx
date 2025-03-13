716745f55e6bee78c1a62e43a407f63c78f813bb
dabinchiii <81626630+dabinchiii@users.noreply.github.com>
2025-03-14 02:58:52 +0900

    Feat: 타이머 기능 구현

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,14 +1,29 @@
+"use client";
+
 import TimerButton from "./TimerButton";
 
-const Timer = () => {
-  const timeLeft = "03:00";
+import useTimer from "@/hooks/useTimer";
+
+const Timer = (): React.ReactNode => {
+  const {
+    // timeLeft,
+    formattedTimeLeft,
+    // isActive,
+    startTimer,
+    pauseTimer,
+    resetTimer,
+  } = useTimer({
+    initialMinutes: 5,
+    onFinish: () => {},
+  });
+
   return (
     <div className="p-4 border flex flex-col gap-8 p-8 mx-auto w-[500px]">
-      <div>{timeLeft}</div>
+      <div>{formattedTimeLeft}</div>
       <div>
-        <TimerButton>start</TimerButton>
-        <TimerButton>pause</TimerButton>
-        <TimerButton>reset</TimerButton>
+        <TimerButton onClick={startTimer}>start</TimerButton>
+        <TimerButton onClick={pauseTimer}>pause</TimerButton>
+        <TimerButton onClick={() => resetTimer()}>reset</TimerButton>
       </div>
     </div>
   );
