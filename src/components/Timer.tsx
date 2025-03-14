2500f9562757849d791e6d22dae8701f2010ab4d
dabinchiii <81626630+dabinchiii@users.noreply.github.com>
2025-03-14 16:56:30 +0900

    Feat: 타이머에 세션 버튼 적용

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -12,8 +12,9 @@ import "react-circular-progressbar/dist/styles.css";
 
 import TimerButton from "./TimerButton";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import useTimer from "@/hooks/useTimer";
+import SessionButton from "./SessionButton";
 
 const modak = Modak({
   variable: "--font-modak",
@@ -46,8 +47,11 @@ const resetIcon = (
   />
 );
 
+type Session = "FOCUS_TIME" | "SHORT_BREAK" | "LONG_BREAK";
+
 const Timer = (): React.ReactNode => {
-  const [targetMinutes] = useState<number>(1);
+  const [targetMinutes, setTargetMinutes] = useState<number>(25);
+  const [selectedSession, setSelectedSession] = useState<Session>("FOCUS_TIME");
 
   const {
     timeLeft,
@@ -61,29 +65,64 @@ const Timer = (): React.ReactNode => {
     onFinish: () => {},
   });
 
+  useEffect(() => {
+    resetTimer(targetMinutes);
+  }, [targetMinutes, resetTimer]);
+
   return (
-    <div className="p-4 border flex flex-col gap-8 p-8 mx-auto w-[500px]">
-      <CircularProgressbarWithChildren
-        value={(timeLeft / (targetMinutes * 60)) * 100}
-        strokeWidth={10}
-        styles={buildStyles({
-          pathColor: "#FFFFFF",
-          trailColor: `rgba(0, 0, 0, 0.3)`,
-          backgroundColor: "#000000",
-        })}
-      >
-        <div
-          className={`${modak.className} font-modak text-white text-8xl [text-shadow:_0_10px_10px_rgb(0_0_0_/_0.25)] `}
+    <>
+      <div className="p-4 border flex flex-col gap-8 p-8 mx-auto w-[500px]">
+        <CircularProgressbarWithChildren
+          value={(timeLeft / (targetMinutes * 60)) * 100}
+          strokeWidth={10}
+          styles={buildStyles({
+            pathColor: "#FFFFFF",
+            trailColor: `rgba(0, 0, 0, 0.3)`,
+            backgroundColor: "#000000",
+          })}
         >
-          {formattedTimeLeft}
-        </div>
-        <div className="flex gap-4">
-          <TimerButton onClick={startTimer}>{startIcon}</TimerButton>
-          <TimerButton onClick={pauseTimer}>{pauseIcon}</TimerButton>
-          <TimerButton onClick={() => resetTimer()}>{resetIcon}</TimerButton>
-        </div>
-      </CircularProgressbarWithChildren>
-    </div>
+          <div
+            className={`${modak.className} font-modak text-white text-8xl [text-shadow:_0_10px_10px_rgb(0_0_0_/_0.25)] `}
+          >
+            {formattedTimeLeft}
+          </div>
+          <div className="flex gap-4">
+            <TimerButton onClick={startTimer}>{startIcon}</TimerButton>
+            <TimerButton onClick={pauseTimer}>{pauseIcon}</TimerButton>
+            <TimerButton onClick={() => resetTimer()}>{resetIcon}</TimerButton>
+          </div>
+        </CircularProgressbarWithChildren>
+      </div>
+      <div className="md:flex gap-8 m-4">
+        <SessionButton
+          title="Focus Time"
+          sessionTime={25}
+          isSelected={selectedSession === "FOCUS_TIME"}
+          onSelected={setTargetMinutes}
+          onClick={() => {
+            setSelectedSession("FOCUS_TIME");
+          }}
+        />
+        <SessionButton
+          title="Short Break"
+          sessionTime={5}
+          isSelected={selectedSession === "SHORT_BREAK"}
+          onSelected={setTargetMinutes}
+          onClick={() => {
+            setSelectedSession("SHORT_BREAK");
+          }}
+        />
+        <SessionButton
+          title="Long Break"
+          sessionTime={15}
+          isSelected={selectedSession === "LONG_BREAK"}
+          onSelected={setTargetMinutes}
+          onClick={() => {
+            setSelectedSession("LONG_BREAK");
+          }}
+        />
+      </div>
+    </>
   );
 };
 
